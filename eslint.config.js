c66dd82bf3e3ea638a668607cf523afbd03c7f4f
qixiaoxin <qixiaoxin@stu.sqxy.edu.cn>
2026-03-03 11:16:41 +0800

    chore: 更新ESLint及相关依赖版本
    更新ESLint至v9.39.3，同时升级@typescript-eslint插件至v8.56.1
    在benchmark工作流中添加skip-fetch-gh-pages配置

---
+++ eslint.config.js
@@ -0,0 +1,34 @@
+import tseslint from '@typescript-eslint/eslint-plugin';
+import tsparser from '@typescript-eslint/parser';
+import eslint from '@eslint/js';
+
+export default [
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    files: ['**/*.ts'],
+    languageOptions: {
+      parser: tsparser,
+      parserOptions: {
+        ecmaVersion: 2022,
+        sourceType: 'module',
+      },
+      globals: {
+        process: 'readonly',
+        Buffer: 'readonly',
+        __dirname: 'readonly',
+        __filename: 'readonly',
+      },
+    },
+    rules: {
+      '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_', varsIgnorePattern: '^_' }],
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+      '@typescript-eslint/no-explicit-any': 'warn',
+      'no-console': 'off',
+    },
+  },
+  {
+    ignores: ['dist/', 'node_modules/', '*.js'],
+  },
+];
